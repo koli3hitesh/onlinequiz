ca2d6b0f030e84f66b4ac74143b15d6a3fffd849
vinikajain <vinikajain.07@gmail.com>
2018-09-28 10:47:25 +0530

    vini

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,10 +6,18 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { StartComponent } from './components/start/start.component';
 import { CrudComponent } from './components/crud/crud.component';
 import { CreateComponent } from './components/crud/create/create.component';
-import { UpdateComponent } from './components/crud/update/update.component';
-import { DeleteComponent } from './components/crud/delete/delete.component';
+
 import { FooterComponent } from './components/footer/footer.component';
 import { CategoryComponent } from './components/category/category.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule, MatOptionModule, MatSelectModule} from '@angular/material';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { ModifyComponent } from './components/crud/modify/modify.component';
+
 
 @NgModule({
   declarations: [
@@ -18,13 +26,26 @@ import { CategoryComponent } from './components/category/category.component';
     StartComponent,
     CrudComponent,
     CreateComponent,
-    UpdateComponent,
-    DeleteComponent,
+  
     FooterComponent,
-    CategoryComponent
+    CategoryComponent,
+    ModifyComponent,
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatCardModule
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
